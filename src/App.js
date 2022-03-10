1698e0a39fd3f2e29d4e7b0f038db9ebcdc1f853
serhii <rootfellenua@gmail.com>
2022-03-10 17:30:17 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,211 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState, useRef } from "react";
+import * as tt from "@tomtom-international/web-sdk-maps";
+import * as ttapi from "@tomtom-international/web-sdk-services";
+import "./App.css";
+import "@tomtom-international/web-sdk-maps/dist/maps.css";
 
-function App() {
+const App = () => {
+  const mapElement = useRef();
+  const [map, setMap] = useState({});
+  const [longitude, setLongitude] = useState(0);
+  const [latitude, setLatitude] = useState(0);
+
+  //? CONVERTING LNG, LAT
+
+  const convertToPoints = (lngLat) => {
+    return {
+      point: {
+        latitude: lngLat.lat,
+        longitude: lngLat.lng,
+      },
+    };
+  };
+
+  //? DRAWING ROUTE
+
+  const drawRoute = (geoJson, map) => {
+    if (map.getLayer("route")) {
+      map.removeLayer("route");
+      map.removeSource("route");
+    }
+    map.addLayer({
+      id: "route",
+      type: "line",
+      source: {
+        type: "geojson",
+        data: geoJson,
+      },
+      paint: {
+        "line-color": "#4a90e2",
+        "line-width": 6,
+      },
+    });
+  };
+
+  //? ADDING DELIVERY MARKER
+
+  const addDeliveryMarker = (lngLat, map) => {
+    const element = document.createElement("div");
+    element.className = "marker-delivery";
+    new tt.Marker({
+      element: element,
+    })
+      .setLngLat(lngLat)
+      .addTo(map);
+  };
+
+  //? ========================================
+  useEffect(() => {
+    const destinations = [];
+
+    //? Setting current location
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setLongitude(position.coords.longitude);
+        setLatitude(position.coords.latitude);
+      },
+      (error) => alert(error.message)
+    );
+    const origin = {
+      lng: longitude,
+      lat: latitude,
+    };
+
+    //? MAP INIT
+    let map = tt.map({
+      key: process.env.REACT_APP_TT_API_KEY,
+      container: mapElement.current,
+      stylesVisibility: {
+        trafficIncidents: true,
+        trafficFlow: true,
+      },
+      center: [longitude, latitude],
+      zoom: 14,
+    });
+    setMap(map);
+
+    //? ADDING MARKER
+
+    const addMarker = () => {
+      const popupOffset = {
+        bottom: [0, -25],
+      };
+      const popup = new tt.Popup({ offset: popupOffset }).setHTML(
+        "You are here!"
+      );
+      const element = document.createElement("div");
+      element.className = "marker";
+      const marker = new tt.Marker({
+        draggable: true,
+        element: element,
+      })
+        .setLngLat([longitude, latitude])
+        .addTo(map);
+
+      marker.on("dragend", () => {
+        const lngLat = marker.getLngLat();
+        setLongitude(lngLat.lng);
+        setLatitude(lngLat.lat);
+      });
+
+      marker.setPopup(popup).togglePopup();
+    };
+
+    addMarker();
+
+    //? SORTING DESTINATIONS
+
+    const sortDestinations = (locations) => {
+      const pointsForDestinations = locations.map((destination) => {
+        return convertToPoints(destination);
+      });
+      const callParameters = {
+        key: process.env.REACT_APP_TT_API_KEY,
+        destinations: pointsForDestinations,
+        origins: [convertToPoints(origin)],
+      };
+      return new Promise((resolve, reject) => {
+        ttapi.services
+          .matrixRouting(callParameters)
+          .then((matrixAPIResults) => {
+            const results = matrixAPIResults.matrix[0];
+            const resultsArray = results.map((result, index) => {
+              return {
+                location: locations[index],
+                drivingTime: result.response.routeSummary.travelTimeInSeconds,
+              };
+            });
+            resultsArray.sort((a, b) => {
+              return a.drivingTime - b.drivingTime;
+            });
+            const sortedLocations = resultsArray.map((result) => {
+              return result.location;
+            });
+            resolve(sortedLocations);
+          });
+      });
+    };
+
+    //? RECALCULATING ROUTES
+
+    const recalculateRoutes = () => {
+      sortDestinations(destinations).then((sorted) => {
+        sorted.unshift(origin);
+
+        ttapi.services
+          .calculateRoute({
+            key: process.env.REACT_APP_TT_API_KEY,
+            locations: sorted,
+          })
+          .then((routeData) => {
+            const geoJson = routeData.toGeoJson();
+            drawRoute(geoJson, map);
+          });
+      });
+    };
+
+    map.on("click", (e) => {
+      destinations.push(e.lngLat);
+      addDeliveryMarker(e.lngLat, map);
+      recalculateRoutes();
+    });
+
+    return () => map.remove();
+  }, [longitude, latitude]);
+
+  //TODO: INITIALIZING MAP
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {map && (
+        <div className="App">
+          <div ref={mapElement} className="map" />
+          <div className="search-bar">
+            <h1 className="search-bar--title">Where to?</h1>
+            <div className="input__container">
+              <input
+                type="number"
+                id="longitude"
+                className="longitude"
+                placeholder="Put in Longitude"
+                onChange={(e) => {
+                  setLongitude(e.target.value);
+                }}
+              />
+              <input
+                type="number"
+                id="latitude"
+                className="latitude"
+                placeholder="Put in Latitude"
+                onChange={(e) => {
+                  setLatitude(e.target.value);
+                }}
+              />
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
-}
+};
 
 export default App;
